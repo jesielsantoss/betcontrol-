b19d7d1f4b00426dd8f35c32eed8a0a19ce953de
jesielsantoss <jesielsantos80@gmail.com>
2026-03-09 01:40:53 -0300

    football.js

--- api/football.js
+++ api/football.js
@@ -1,1 +1,14 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
 
+  const path = req.query.path
+  if (!path) return res.status(400).json({ error: 'Missing path' })
+
+  const url = `https://v3.football.api-sports.io${path}`
+  const response = await fetch(url, {
+    headers: { 'x-apisports-key': process.env.FOOTBALL_API_KEY }
+  })
+  const data = await response.json()
+  res.status(200).json(data)
+}
